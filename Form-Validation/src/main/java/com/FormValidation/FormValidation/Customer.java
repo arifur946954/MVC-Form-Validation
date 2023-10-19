65be01ef02571f32851e71f397b2e5bb4ba99d6f
arifur946954 <arif.bd08060@gmail.com>
2023-10-19 17:03:55 +0600

    check spaces

--- Form-Validation/src/main/java/com/FormValidation/FormValidation/Customer.java
+++ Form-Validation/src/main/java/com/FormValidation/FormValidation/Customer.java
@@ -1,21 +1,29 @@
 package com.FormValidation.FormValidation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
     private String firstName;
     @NotNull(message = "last name must not be empty")
-    @Size(min = 1 ,message="last name contain 5 to 10 characters")
+    @Size(min = 5 ,message="last name contain 5 to 10 characters")
     private String lastName;
+    @Max(value = 10,message = "value must less then 10")
+    @Min(value = 1,message = "value must be grater than 1")
+    private String freeSpace;
 
     public Customer() {
     }
 
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, String freeSpace) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.freeSpace = freeSpace;
     }
+
+
 
     public String getFirstName() {
         return firstName;
@@ -33,11 +41,20 @@ public class Customer {
         this.lastName = lastName;
     }
 
+    public String getFreeSpace() {
+        return freeSpace;
+    }
+
+    public void setFreeSpace(String freeSpace) {
+        this.freeSpace = freeSpace;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", freeSpace='" + freeSpace + '\'' +
                 '}';
     }
 }
